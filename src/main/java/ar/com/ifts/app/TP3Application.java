45b82d93351e3b7c84b1a40409b7ca02455190f9
alexandervaca.alexvaca <alexandervaca.alex_andy_25@yahoo.com.ar>
2020-11-07 00:21:40 -0300

    Se cambia puerto para app angular

--- src/main/java/ar/com/ifts/app/TP3Application.java
+++ src/main/java/ar/com/ifts/app/TP3Application.java
@@ -26,7 +26,7 @@ public class TP3Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
