325a7cbc5b536be925cf21aae027838a62fdbff9
fbonifac94 <facu.bonifacich@hotmail.com>
2020-11-08 19:06:08 -0300

    Corrección inicialización de properties del modelo de usuario.

--- src/main/java/ar/com/ifts/app/model/Usuario.java
+++ src/main/java/ar/com/ifts/app/model/Usuario.java
@@ -145,17 +145,17 @@ public class Usuario implements UserDetails {
 
 	@Override
 	public boolean isAccountNonExpired() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		return false;
+		return true;
 	}
 
 	@Override
